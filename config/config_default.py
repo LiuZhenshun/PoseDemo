26b66f6c0f72581518ba4f218b530fd1191ad004
Liu Zhenshun <zhenshunliu1998@gmail.com>
2023-02-10 15:39:14 +0800

    runrealtime

--- config/config_default.py
+++ config/config_default.py
@@ -1,18 +1,23 @@
 
 
-detector_cfg = "/home/hkuit155/Downloads/ThermalTemData/testDetection_xjl/testModel/yolov3/yolo_tiny/yolov3-tiny-1cls.cfg"
-detector_weight = "/home/hkuit155/Downloads/ThermalTemData/testDetection_xjl/testModel/yolov3/yolo_tiny/backup90.pt"
+detector_cfg = "/media/hkuit164/Backup/OneDrive_1_2-9-2023/yolov3-spp-1cls.cfg"
+detector_weight = "/media/hkuit164/Backup/OneDrive_1_2-9-2023/backup90.pt"
 
-pose_weight = "/home/hkuit155/Desktop/ThermalProject/PoseTrainingPytorch/exp/default/default/20.pth"
-pose_model_cfg = "/home/hkuit155/Downloads/ThermalTemData/testDetection_xjl/testModel/alphapose/model_cfg.json"
-pose_data_cfg = "/home/hkuit155/Downloads/ThermalTemData/testDetection_xjl/testModel/alphapose/data_cfg.json"
+RgbVideoCap = 'rtsp://admin:fyp202020@192.168.8.111:554/Streaming/Channels/101/?transportmode=unicast --input-rtsp-latency=0'
+TherVideoCap = 'rtsp://admin:fyp202020@192.168.8.111:554/Streaming/Channels/201/?transportmode=unicast --input-rtsp-latency=0'
 
-input_src = "/home/hkuit155/Downloads/ThermalTemData/testDetection_xjl/testModel/inputVideo.mp4"
-output_src = "/home/hkuit155/Downloads/ThermalTemData/testDetection_xjl/testModel/inputVideo_output.mp4"
+pose_weight = "/media/hkuit164/Backup/latest.pth"
+pose_model_cfg = ""
+pose_data_cfg = ""
+
+#input_src = TherVideoCap
+input_src = "/media/hkuit164/Backup/free_1_thermal (online-video-cutter.com).mp4"
+output_src = "/media/hkuit164/Backup/free_1_thermal (online-video-cutter.com)_detectionresult.mp4"
 out_size = (1080, 720)
 show_size = (1080, 720)
-show = False
+show = True
 
-write_json = True
+write_json = False
 json_path = ""
 filter_criterion = {}
+
